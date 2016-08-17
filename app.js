70f0bf4f9ef8f2c65e2ab4b60b975c72be979d33
Sang Theresa Lee <tsahlee@gmail.com>
2016-08-17 08:34:22 -0400

    1.3

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var app = angular.module('store', []);
+(function () {
+ var app = angular.module('store', []);
+
+ var gem = {
+  description: "...",
+   name: 'Dodecahedron',
+   price: 2.95,
+   canPurchase: false;
+ };
+
+ app.controller('StoreController', function () {
+   this.product = gem;
+ });
+
+})();
+
